bd0cdb6dd53be07764a736171ecfd3644182f3fe
raipr <q2494301@gmail.com>
2026-07-16 11:50:08 +0530

    Revert package.json scripts to standard vite after switching to user-space Node 20 environment

--- polyfill.js
+++
@@ -1,9 +0,0 @@
-// Polyfill CustomEvent for Node.js versions older than v19 (such as Node v18 on Jetson)
-if (typeof globalThis.CustomEvent === 'undefined') {
-  globalThis.CustomEvent = class CustomEvent extends Event {
-    constructor(type, dict = {}) {
-      super(type, dict);
-      this.detail = dict.detail || null;
-    }
-  };
-}
